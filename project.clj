2e7a64be97f760172081988d7d306dd72aaff4f6
Markus Walther <m.walther97@gmail.com>
2019-12-31 01:25:18 +0100

    fix license URL

--- project.clj
+++ project.clj
@@ -1,8 +1,9 @@
 (defproject av-clj "0.1.0-SNAPSHOT"
   :description "Audio Visual stuff with Shadertone / GLSL"
   :url "http://github.com/markus-wa/av-clj"
-  :license {:name "MIT"
-            :url "https://www.eclipse.org/legal/epl-2.0/"}
+  :license
+  {:name "MIT"
+   :url  "https://opensource.org/licenses/MIT"}
   :dependencies [[org.clojure/clojure "1.10.0"]
                  [overtone "0.10.6" :exclusions [[clj-native]]]
                  [shadertone "0.2.6-SNAPSHOT"]
